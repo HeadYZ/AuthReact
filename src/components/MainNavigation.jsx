64881a5539d6ce233c53db624c1226c82dd8d445
Patryk <krasowski.p@outlook.com>
2024-06-15 22:15:47 +0200

    add authentication

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -31,22 +31,22 @@ function MainNavigation() {
           </li>
           <li>
             <NavLink
-              to="/auth"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Authentication
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
               to="/newsletter"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
             >
               Newsletter
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/auth?mode=login"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Authentication
             </NavLink>
           </li>
         </ul>
